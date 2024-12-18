a119971c1915b644a2c2cc4a0e4b24afbdcaa429
parkernowicki <parkernowicki@gmail.com>
2024-12-17 21:07:06 -0700

    Tree generation and growth on blocks other than dirt!

--- src/main/java/com/parkern/firstmod/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/parkern/firstmod/worldgen/ModPlacedFeatures.java
@@ -1,9 +1,12 @@
 package com.parkern.firstmod.worldgen;
 
 import com.parkern.firstmod.FirstMod;
+import com.parkern.firstmod.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -19,6 +22,8 @@ public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> NETHER_TITANITE_ORE_PLACED_KEY = registerKey("nether_titanite_ore_placed");
     public static final ResourceKey<PlacedFeature> END_TITANITE_ORE_PLACED_KEY = registerKey("end_titanite_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> BLOODWOOD_PLACED_KEY = registerKey("bloodwood_placed");
+
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
@@ -28,6 +33,10 @@ public class ModPlacedFeatures {
                 ModOrePlacement.commonOrePlacement(12, HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
         register(context, END_TITANITE_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_TITANITE_ORE_KEY),
                 ModOrePlacement.commonOrePlacement(12, HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
+
+        register(context, BLOODWOOD_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.BLOODWOOD_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
+                        ModBlocks.BLOODWOOD_SAPLING.get()));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
