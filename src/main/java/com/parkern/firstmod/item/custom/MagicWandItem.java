28a2518ec951f2013e63bd192749ec196c21cc69
parkernowicki <parkernowicki@gmail.com>
2025-02-11 17:58:18 -0700

    Spawning particles!

--- src/main/java/com/parkern/firstmod/item/custom/MagicWandItem.java
+++ src/main/java/com/parkern/firstmod/item/custom/MagicWandItem.java
@@ -4,6 +4,8 @@ import com.parkern.firstmod.block.ModBlocks;
 import com.parkern.firstmod.component.ModDataComponents;
 import com.parkern.firstmod.sound.ModSounds;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.core.particles.BlockParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
@@ -52,6 +54,10 @@ public class MagicWandItem extends Item {
                         item -> context.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
 
                 level.playSound(null, context.getClickedPos(), ModSounds.MAGIC_WAND_USE.get(), SoundSource.BLOCKS);
+
+                ((ServerLevel) level).sendParticles(new BlockParticleOption(ParticleTypes.BLOCK, clickedBlock.defaultBlockState()),
+                        context.getClickedPos().getX() + 0.5, context.getClickedPos().getY() + 1.0,
+                        context.getClickedPos().getZ() + 0.5, 5, 0, 0, 0, 1);
 
                 context.getItemInHand().set(ModDataComponents.COORDINATES, context.getClickedPos());
             }
