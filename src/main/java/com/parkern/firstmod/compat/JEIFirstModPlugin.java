62be3fb1838bd67061c5ef20fdb89b6f98632cf0
parkernowicki <parkernowicki@gmail.com>
2025-03-31 17:34:51 -0600

    JEI compatibility!

---
+++ src/main/java/com/parkern/firstmod/compat/JEIFirstModPlugin.java
@@ -0,0 +1,55 @@
+package com.parkern.firstmod.compat;
+
+import com.parkern.firstmod.FirstMod;
+import com.parkern.firstmod.block.ModBlocks;
+import com.parkern.firstmod.recipe.GrowthChamberRecipe;
+import com.parkern.firstmod.recipe.ModRecipes;
+import com.parkern.firstmod.screen.custom.GrowthChamberScreen;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+
+import java.util.List;
+
+@JeiPlugin
+public class JEIFirstModPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(FirstMod.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new GrowthChamberRecipeCategory(
+                registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
+
+        List<GrowthChamberRecipe> growthChamberRecipes = recipeManager
+                .getAllRecipesFor(ModRecipes.GROWTH_CHAMBER_TYPE.get()).stream().map(RecipeHolder::value).toList();
+        registration.addRecipes(GrowthChamberRecipeCategory.GROWTH_CHAMBER_RECIPE_RECIPE_TYPE, growthChamberRecipes);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        registration.addRecipeClickArea(GrowthChamberScreen.class, 74, 30, 22, 20,
+                GrowthChamberRecipeCategory.GROWTH_CHAMBER_RECIPE_RECIPE_TYPE);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.GROWTH_CHAMBER.asItem()),
+                GrowthChamberRecipeCategory.GROWTH_CHAMBER_RECIPE_RECIPE_TYPE);
+    }
+}
